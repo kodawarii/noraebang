36072d0cbcc7ba514207afad67cce06abb619ddc
risou <ayamachi@outlook.com>
2019-07-20 15:59:25 +1200

    Updates

--- precache-manifest.3b5a2d050e10533200c636aec6e2150f.js
+++ precache-manifest.3b5a2d050e10533200c636aec6e2150f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0562e47060c1652b9a27827dffa94778",
+    "revision": "eb469bc08112e50ae450731fd0cfbbd3",
     "url": "/noraebang/index.html"
   },
   {
-    "revision": "b72fda2345cd1de35c93",
-    "url": "/noraebang/static/css/main.893c331d.chunk.css"
+    "revision": "7a7139aec7f5c0313af0",
+    "url": "/noraebang/static/css/main.2044c90a.chunk.css"
   },
   {
     "revision": "e03c5abe89bbb3596305",
     "url": "/noraebang/static/js/2.b07c22ef.chunk.js"
   },
   {
-    "revision": "b72fda2345cd1de35c93",
-    "url": "/noraebang/static/js/main.68770775.chunk.js"
+    "revision": "7a7139aec7f5c0313af0",
+    "url": "/noraebang/static/js/main.44f5a132.chunk.js"
   },
   {
     "revision": "b46e298961b4f6a6ab20",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/noraebang/precache-manifest.06b0798fba378b334a16a1867e9b50cf.js"
+  "/noraebang/precache-manifest.3b5a2d050e10533200c636aec6e2150f.js"
 );
 
 self.addEventListener('message', (event) => {
